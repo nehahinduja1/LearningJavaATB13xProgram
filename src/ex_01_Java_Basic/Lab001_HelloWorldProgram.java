e4323461a923b60983ea3b93afe63b9036c12893
ATTAQA <nehahinduja29@gmail.com>
2025-06-09 20:34:54 +0100

    9th June 2025

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class Lab001_HelloWorld {
+public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Hello, I am RUNNING JAVA Program");
     }
--- src/ex_01_Java_Basic/Lab003_ShortcutHW.java
+++ src/ex_01_Java_Basic/Lab003_ShortcutHW.java
@@ -5,6 +5,10 @@ public class Lab003_ShortcutHW {
         System.out.println("Hello Test");
 
     }
+
+//    public static void main(String[} args {
+//    }
+
  public static void main(int args){
 
  }
