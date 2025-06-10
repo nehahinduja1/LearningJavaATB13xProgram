b31f59ce192e2807090c77777fb699bffc362f63
ATTAQA <nehahinduja29@gmail.com>
2025-06-10 20:28:26 +0100

    10th June 2025 task

---
+++ src/ex_02_Java_Basics/Lab027_Task.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics;
+
+public class Lab027_Task {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 1;
+        System.out.printf("%d * %d = %d%n" , a, b, a* 1 );
+        System.out.printf("%d * %d = %d%n", a,b , a*2);
+        System.out.printf("%d * %d = %d%n", a,b , a*3);
+        System.out.printf("%d * %d = %d%n", a,b , a*4);
+        System.out.printf("%d * %d = %d%n", a,b , a*5);
+        System.out.printf("%d * %d = %d%n", a,b , a*6);
+        System.out.printf("%d * %d = %d%n", a,b , a*7);
+        System.out.printf("%d * %d = %d%n", a,b , a*8);
+        System.out.printf("%d * %d = %d%n", a,b , a*9);
+        System.out.printf("%d * %d = %d%n", a,b , a*10);
+
+    }
+}
