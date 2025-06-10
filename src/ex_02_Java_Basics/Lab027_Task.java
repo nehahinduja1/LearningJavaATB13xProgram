05365270b8c6bd070c2aa62757ec626179ddb43f
ATTAQA <nehahinduja29@gmail.com>
2025-06-10 20:37:38 +0100

    10th June 2025 task updated

--- src/ex_02_Java_Basics/Lab027_Task.java
+++ src/ex_02_Java_Basics/Lab027_Task.java
@@ -5,15 +5,15 @@ public class Lab027_Task {
         int a = 9;
         int b = 1;
         System.out.printf("%d * %d = %d%n" , a, b, a* 1 );
-        System.out.printf("%d * %d = %d%n", a,b , a*2);
-        System.out.printf("%d * %d = %d%n", a,b , a*3);
-        System.out.printf("%d * %d = %d%n", a,b , a*4);
-        System.out.printf("%d * %d = %d%n", a,b , a*5);
-        System.out.printf("%d * %d = %d%n", a,b , a*6);
-        System.out.printf("%d * %d = %d%n", a,b , a*7);
-        System.out.printf("%d * %d = %d%n", a,b , a*8);
-        System.out.printf("%d * %d = %d%n", a,b , a*9);
-        System.out.printf("%d * %d = %d%n", a,b , a*10);
+        System.out.printf("%d * %d = %d%n", a,b+1 , a*2);
+        System.out.printf("%d * %d = %d%n", a,b+2 , a*3);
+        System.out.printf("%d * %d = %d%n", a,b+3 , a*4);
+        System.out.printf("%d * %d = %d%n", a,b+4 , a*5);
+        System.out.printf("%d * %d = %d%n", a,b+5 , a*6);
+        System.out.printf("%d * %d = %d%n", a,b+6 , a*7);
+        System.out.printf("%d * %d = %d%n", a,b+7 , a*8);
+        System.out.printf("%d * %d = %d%n", a,b+8 , a*9);
+        System.out.printf("%d * %d = %d%n", a,b+9 , a*10);
 
     }
 }
