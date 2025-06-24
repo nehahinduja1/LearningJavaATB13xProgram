14affb4349ada2afdf291eb314793b38bcb2c904
ATTAQA <nehahinduja29@gmail.com>
2025-06-24 12:14:31 +0100

    23th June 2025

--- src/ex_14_Strings/InterviewCoding.java
+++ src/ex_14_Strings/InterviewCoding.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 package ex_14_Strings;
 
 public class InterviewCoding {
@@ -10,8 +11,27 @@ public class InterviewCoding {
         // G
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String input = scanner.next();
+        // String newString_reversed = reverseStringSB(input);
+        String newString_reversed = reverseString(input);
+
+        if (newString_reversed.equalsIgnoreCase(input)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Non palindrome");
+        }
+        // madam.charAt(1) -> a
+    }
+
+    Static String reverseString(String userInput) {
+        String reversed = "";
+        for (int i = userInput.Length )
+    }
 
 
 
+
+    private static String reverseString(String input) {
     }
 }
