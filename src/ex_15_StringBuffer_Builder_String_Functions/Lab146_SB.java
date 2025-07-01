733742f11993b02c606771a109bbd55b4d84230d
ATTAQA <nehahinduja29@gmail.com>
2025-07-01 11:28:57 +0100

    30th June 2025

---
+++ src/ex_15_StringBuffer_Builder_String_Functions/Lab146_SB.java
@@ -0,0 +1,10 @@
+package ex_15_StringBuffer_Builder_String_Functions;
+
+public class Lab146_SB {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Pramod");
+        stringBuilder.append(123);
+
+        System.out.println(stringBuilder);
+    }
+}
