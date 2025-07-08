2810ef56e0ede1683b515b5025aca658293b643f
ATTAQA <nehahinduja29@gmail.com>
2025-07-08 22:50:56 +0100

    03rd July 2025

--- src/ex_21_OOPs_Encapsulation/Lab190_REAL_Bank.java
+++ src/ex_21_OOPs_Encapsulation/Lab190_REAL_Bank.java
@@ -11,6 +11,12 @@ public class Lab190_REAL_Bank {
     }
 }
 class ICICIBank {
+
+    //- Encapsulation is a fundamental concept in object-oriented programming (OOP).
+    //- Bundling of data and methods that operate on that data within a single unit.
+    //- Data members should be private in nature.
+    //- Methods with getter and setter only, you can access the data members / attributes.
+
     private String name;
     private long bal;
 
