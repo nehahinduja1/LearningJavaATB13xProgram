56b071938d8c977e9078089ef76f7efec27dd719
ATTAQA <nehahinduja29@gmail.com>
2025-07-23 18:25:30 +0100

    24 June

--- src/ex_16_Arrays/Lab153_Arrays_Max_Min.java
+++ src/ex_16_Arrays/Lab153_Arrays_Max_Min.java
@@ -3,18 +3,47 @@ package ex_16_Arrays;
 import java.util.Arrays;
 
 public class Lab153_Arrays_Max_Min {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // find the max element within array
 
-        int[] array = {25, 16, 14, 87, 94, 68, 75, 48};
-        Arrays.sort(array);  // ascending to descending // 14, 16, 25, 48, 68, 75, 87, 94 = 8-1 = 7
+        int[] n = {25, 16, 14, 87, 94, 68, 75, 48};  // 0 to 7
+        Arrays.sort(n);  // ascending to descending // 25, 16, 14, 87, 94, 68, 75, 48 = 8-1 = 7
 //       Print 7th value i.e 94
-//        System.out.println(array[array.length - 1]);
+//      System.out.println(number[number.length - 1]);
+//        System.out.println(number[7]);
+
+        int max_number = number(n);
+        int min_number = number1(n);
+        System.out.println(max_number);
+        System.out.println(min_number);
 
     }
 
+    static int number(int[] n) {
+        int max = n[0];
+        // logic
 
+        for (int i = 0; i < n.length; i++) {
+            if (n[i] > max) {
+                max = n[i];
+            }
+        }
+        return max;
 
+    }
+
+    static int number1 (int[] n) {
+        int min = n[0];
+        // logic
+
+        for (int i = 0; i < n.length; i++) {
+            if (n[i] < min) {
+                min = n[i];
+            }
+        }
+        return min;
+
+    }
 
 
 
---
+++ src/ex_16_Arrays/Lab154_Array_User_Inputs.java
@@ -0,0 +1,15 @@
+package ex_16_Arrays;
+
+import java.util.Scanner;
+
+public class Lab154_Array_User_Inputs {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array int only");
+        int size = sc.nextInt();
+
+
+
+    }
+}
